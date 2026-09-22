098975cc26e8dc1a89362d6ad9031733a1155a15
MRITUNJAY NAMDEO <158102742+JhakaasJai@users.noreply.github.com>
2026-09-23 00:21:21 +0530

    Create 1054-ComplementOfBase10Integer

---
+++ solutions/1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.java
@@ -0,0 +1,17 @@
+// Last updated: 9/23/2026, 12:21:17 AM
+class Solution {
+    public int bitwiseComplement(int n) {
+        int mask=0;
+        int m=n;
+        if(n==0){
+            return 1;
+        }
+        while(m!=0){
+            mask=(mask<<1)+1;
+            m=m>>1;
+        }
+        
+
+        return n^mask;
+    }
+}
