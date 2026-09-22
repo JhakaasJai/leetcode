821c5930a6d63c6b22e410b6b34996d6b6960b90
MRITUNJAY NAMDEO <158102742+JhakaasJai@users.noreply.github.com>
2026-09-23 00:21:40 +0530

    Create 204-CountPrimes

---
+++ solutions/204-CountPrimes/204-CountPrimes.cpp
@@ -0,0 +1,28 @@
+// Last updated: 9/23/2026, 12:21:37 AM
+class Solution {
+public:
+    int countPrimes(int n){ 
+  
+  if(n<2){
+    return 0;
+  }
+  vector<int> arr(n,1);
+  arr[0]=0;
+  arr[1]=0;
+  for(int i=2; i*i<n; i++){
+    if(arr[i]){
+      for(int j=i*i; j<n; j+=i){
+        arr[j]=0;
+      }
+    }
+  }
+
+  int count=0;
+  for(int k: arr){
+    if(k==1){
+      count++;
+    }
+  }
+  return count;
+}
+};
