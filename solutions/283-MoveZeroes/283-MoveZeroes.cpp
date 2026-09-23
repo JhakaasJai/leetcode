c43315986f25eac0adc85feec8fcb291ba880a22
MRITUNJAY NAMDEO <158102742+JhakaasJai@users.noreply.github.com>
2026-09-24 00:13:11 +0530

    File updated at 9/24/2026, 12:13:09 AM

--- solutions/283-MoveZeroes/283-MoveZeroes.cpp
+++ solutions/283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,14 +1,15 @@
-// Last updated: 9/23/2026, 12:21:26 AM
+// Last updated: 9/24/2026, 12:13:09 AM
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        int j=0;
-        for(int i=0; i<nums.size();i++){
-            if(nums[i]!=0){
-                swap(nums[i], nums[j]);
-                j++;
+        int i=0;
+        for(int j=0; j<nums.size(); j++){
+            if(nums[i]==0){
+                nums.erase(nums.begin()+i);
+                nums.push_back(0);
+                continue;
             }
+            i++;
         }
     }
 };
