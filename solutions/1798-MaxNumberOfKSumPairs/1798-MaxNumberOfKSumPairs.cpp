925c8fe8b3bbfa2ff2af466a278c7535c6787acd
MRITUNJAY NAMDEO <158102742+JhakaasJai@users.noreply.github.com>
2026-09-25 01:06:46 +0530

    Create 1798-MaxNumberOfKSumPairs

---
+++ solutions/1798-MaxNumberOfKSumPairs/1798-MaxNumberOfKSumPairs.cpp
@@ -0,0 +1,26 @@
+// Last updated: 9/25/2026, 1:06:44 AM
+class Solution {
+public:
+
+
+    int maxOperations(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+
+        int count=0;
+        int i=0, j=nums.size()-1;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                i++;
+                j--;
+                count++;
+            } else if(nums[i]+nums[j]>k){
+                j--;
+            } else{
+                i++;
+            }
+        }
+        return count;
+       
+    }
+};
